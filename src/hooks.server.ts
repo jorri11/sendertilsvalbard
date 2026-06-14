c2507dbe54c45bf39e5562a1a9c6cf39dafe6510
Jørgen Bang Sneisen <jorgen.sneisen@lf-nett.no>
2026-06-14 21:44:59 +0200

    Remove www redirect

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,15 +5,6 @@ import { redirect, type Handle } from "@sveltejs/kit";
 let bootstrapComplete = false;
 
 export const handle: Handle = async ({ event, resolve }) => {
-  const host = event.url.hostname;
-  console.log(host, host == "www.sendertilsvalbard.no");
-  if (host === "www.sendertilsvalbard.no") {
-    console.log("wwww redirect");
-    throw redirect(
-      301,
-      `https://sendertilsvalbard.no${event.url.pathname}${event.url.search}`,
-    );
-  }
   if (!bootstrapComplete) {
     migrateDatabase();
     bootstrapAdminUser();
