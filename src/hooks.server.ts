080ed7535f15b2b2ef2e10fb3c8b8c2065eb39a7
Jørgen Bang Sneisen <jorgen.sneisen@lf-nett.no>
2026-06-14 21:30:44 +0200

    Handle www redirect

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,24 @@
-import { bootstrapAdminUser, getUserFromSession } from '$lib/server/auth';
-import { migrateDatabase } from '$lib/server/db';
-import type { Handle } from '@sveltejs/kit';
+import { bootstrapAdminUser, getUserFromSession } from "$lib/server/auth";
+import { migrateDatabase } from "$lib/server/db";
+import { redirect, type Handle } from "@sveltejs/kit";
 
 let bootstrapComplete = false;
 
 export const handle: Handle = async ({ event, resolve }) => {
+  const host = event.url.hostname;
+
+  if (host === "www.sendertilsvalbard.no") {
+    throw redirect(
+      301,
+      `https://sendertilsvalbard.no${event.url.pathname}${event.url.search}`,
+    );
+  }
   if (!bootstrapComplete) {
     migrateDatabase();
     bootstrapAdminUser();
     bootstrapComplete = true;
   }
 
-  event.locals.user = getUserFromSession(event.cookies.get('session'));
+  event.locals.user = getUserFromSession(event.cookies.get("session"));
   return resolve(event);
 };
